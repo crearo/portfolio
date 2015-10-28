4c95696e166b2808ac5c7bdd2c79830c28d54513
CreaRo <bhardwaj.rish@gmail.com>
2015-10-28 23:48:58 +0530

    Added Portfolio

--- run.py
+++ run.py
@@ -44,7 +44,49 @@ class Weblog(db.Model):
 		self.w_category = w_category
 		self.w_weight = w_weight
 
-@app.route('/')
+class Job(db.Model):
+	id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+	j_company =  db.Column(db.String)
+	j_title =  db.Column(db.String)
+	j_startdate =  db.Column(db.String)
+	j_enddate =  db.Column(db.String)
+	j_duration =  db.Column(db.String)
+	j_description =  db.Column(db.String)
+	j_link =  db.Column(db.String)
+
+	def __init__(self, j_company,j_title,j_startdate,j_enddate,j_duration,j_description,j_link):
+		self.j_company = j_company
+		self.j_title = j_title
+		self.j_startdate = j_startdate
+		self.j_enddate = j_enddate
+		self.j_duration = j_duration
+		self.j_description = j_description
+		self.j_link = j_link
+
+class Eduaction(db.Model):
+	id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+	e_name =  db.Column(db.String)
+	e_degree =  db.Column(db.String)
+	e_major =  db.Column(db.String)
+	e_description =  db.Column(db.String)
+	e_startdate =  db.Column(db.String)
+	e_enddate =  db.Column(db.String)
+	e_link =  db.Column(db.String)
+
+	def __init__(self,e_name, e_degree,e_major,e_description,e_startdate,e_enddate,	e_link):
+		self.e_name = e_name
+		self.e_degree = e_degree
+		self.e_major = e_major
+		self.e_description = e_description
+		self.e_startdate = e_startdate
+		self.e_enddate = e_enddate
+		self.e_link = e_link
+
+@app.route("/")
+def root():
+	return redirect(url_for('home'))
+
+@app.route('/home')
 def home():
 	color = 'blue'
 	title = "Rishabh Bhardwaj"
@@ -55,12 +97,15 @@ def home():
 
 @app.route('/portfolio')
 def portfolio():
+	jobs = Job.query.all()
+	edus = Eduaction.query.all()
+
 	color = 'dark'
 	title = "Portfolio"
 	titleback = "CV"
 	subtitle = "The fox crossed the way"
 	subcontent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-	return render_template('portfolio.html',color = color, title = title, titleback = titleback, subtitle = subtitle, subcontent = subcontent)
+	return render_template('portfolio.html', jobs = jobs, edus = edus, color = color, title = title, titleback = titleback, subtitle = subtitle, subcontent = subcontent)
 
 
 @app.route('/code')
@@ -70,7 +115,7 @@ def code():
 	titleback = "C"
 	subtitle = "The fox crossed the way"
 	subcontent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-	return render_template('home.html', color = color, title = title, titleback = titleback, subtitle = subtitle, subcontent = subcontent)
+	return render_template('code.html', color = color, title = title, titleback = titleback, subtitle = subtitle, subcontent = subcontent)
 
 @app.route('/weblog')
 def weblog():

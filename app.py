c3afc123d200dbd095cd492f33f21e249c7bf34f
Rishabh Bhardwaj <rishabh.bhardwaj@tonboimaging.com>
2018-01-09 08:45:55 +0530

    Minor mod to app.py to test heroku/website reflection time

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-	return "Yo, it's working!"
+	return "Yo, it's working! Here's a minor mod to see the diff to upload to heroku!"
 if __name__ == "__main__":
 	app.run()

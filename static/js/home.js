1f8966a624b7c7668283d3180e9be09e45ff99e3
crearo <rishabh.bhardwaj@tonboimaging.com>
2018-09-27 14:50:36 +0530

    Update timeline + home text

--- static/js/home.js
+++ static/js/home.js
@@ -2,18 +2,18 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> work: android, python, graphics, computer vision.<br/> ^100" +
+      "><span class='caret'>$</span> work: android, python, camera, computer vision.<br/> ^100" +
       "><span class='caret'>$</span> hobbies: football, solo travel, blogging<br/> ^300" +
       "><span class='caret'>$</span> alias: crearo <br/>" +
-      "><span class='caret'>$</span> highlight: <a href='/projects/lifehacks'>app with >750K installs</a>, <a href='/projects/video-stab'>real time image stabilization on Android</a><br/>" +
+      "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>" +
       "><span class='caret'>$</span> job: <a href='http://www.tonboimaging.com/'>Tonbo Imaging</a><br/> ^100" +
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"
     ],
     showCursor: true,
     cursorChar: '_',
     autoInsertCss: true,
-    typeSpeed: 1,
-    startDelay: 200,
+    typeSpeed: 0.001,
+    startDelay: 50,
     loop: false,
     showCursor: false,
     onStart: $('.message form').hide(),

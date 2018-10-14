ea173e1b4445ce3a75a738b5fa1c73ae58466f2c
crearo <rishabh.bhardwaj@tonboimaging.com>
2018-10-14 15:49:18 +0530

    Modify terminal text slightly

--- static/js/home.js
+++ static/js/home.js
@@ -2,12 +2,12 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> work: android, python, camera, computer vision.<br/> ^100" +
+      "><span class='caret'>$</span> work: hardware-software interaction, camera, computer vision<br/> ^100" +
       "><span class='caret'>$</span> hobbies: football, solo travel, blogging<br/> ^300" +
       "><span class='caret'>$</span> alias: crearo <br/>" +
       "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>" +
-      "><span class='caret'>$</span> job: <a href='http://www.tonboimaging.com/'>Tonbo Imaging</a><br/> ^100" +
-      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"
+      "><span class='caret'>$</span> job: military thermal cameras at <a href='http://www.tonboimaging.com/'>Tonbo Imaging</a><br/> ^100" /*+
+      "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
     ],
     showCursor: true,
     cursorChar: '_',

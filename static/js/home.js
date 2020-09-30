d9dcd9a0ea384c1679c2b3033c4405656270e3bc
Vanshi1999 <72151208+Vanshi1999@users.noreply.github.com>
2020-09-30 22:07:49 +0530

    Improved texts

--- static/js/home.js
+++ static/js/home.js
@@ -3,11 +3,11 @@ $(function() {
   $(".typed").typed({
     strings: [
       "stat rish.human<br/>" + 
-      "><span class='caret'>$</span> skills: android, hardware-software interaction, camera, computer vision<br/> ^100" +
-      "><span class='caret'>$</span> job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +
-      "><span class='caret'>$</span> hobbies: football, travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
-      "><span class='caret'>$</span> alias: crearo <br/>" +
-      "><span class='caret'>$</span> highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
+      "><span class='caret'>$</span> Skills: android, hardware-software interaction, camera, computer vision<br/> ^100" +
+      "><span class='caret'>$</span> Job: android auto at <a href='http://www.google.com/'>Google Munich</a><br/> ^100" +
+      "><span class='caret'>$</span> Hobbies: football, travel, <a href='http://www.rish.space/blog'>writing</a><br/> ^300" +
+      "><span class='caret'>$</span> Alias: crearo <br/>" +
+      "><span class='caret'>$</span> Highlight:  <a href='/projects/video-stab'>real time image stabilization on Android</a>, <a href='/projects/lifehacks'>app with >750K installs</a><br/>"/*
       "><span class='caret'>$</span> <a href='/timeline'>timeline</a> <a href='http://www.github.com/crearo/'>github</a> <a href='http://in.linkedin.com/in/bhardwajrish/'>linkedin</a> <a href='http://bhardwajrish.blogspot.com/'>blog</a><br/>"*/
     ],
     showCursor: true,
